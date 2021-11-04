3c3c10588bbd9678655cf966b474dea8ad78535b
KouT127 <kou26haru@gmail.com>
2021-11-04 23:24:29 +0900

    initial commit

---
+++ app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/detail/DetailFragment.kt
@@ -0,0 +1,33 @@
+package com.example.fragmentwithdatabindingplayground.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.fragmentwithdatabindingplayground.R
+import com.example.fragmentwithdatabindingplayground.ui.main.MainFragment
+import com.example.fragmentwithdatabindingplayground.ui.main.MainViewModel
+
+class DetailFragment : Fragment() {
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/main/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.fragmentwithdatabindingplayground.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.fragmentwithdatabindingplayground.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false).apply {
+            findViewById<Button>(R.id.detail_fragment_button).apply {
+                setOnClickListener {
+                    findNavController().navigate(R.id.action_main_fragment_to_detailFragment)
+                }
+            }
+        }
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}

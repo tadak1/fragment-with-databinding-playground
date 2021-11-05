9c29fcbe7835df385bca6dcbafcd643c70455e3b
KouT127 <kou26haru@gmail.com>
2021-11-05 23:54:00 +0900

    modify: Inject dependency

--- app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/main/MainFragment.kt
@@ -6,17 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.fragmentwithdatabindingplayground.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,9 +33,11 @@ class MainFragment : Fragment() {
             }
         }
     }
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.userDetail.observe(viewLifecycleOwner) {
+            print(it)
+        }
     }
 }
--- app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/fragmentwithdatabindingplayground/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.fragmentwithdatabindingplayground.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fragmentwithdatabindingplayground.di.GithubRepository
+import com.example.fragmentwithdatabindingplayground.di.GithubUserDetail
+import com.example.fragmentwithdatabindingplayground.di.Resource
+import com.example.fragmentwithdatabindingplayground.di.Resource.Loading
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: GithubRepository
+) : ViewModel() {
+    val userDetail = MutableLiveData<Resource<GithubUserDetail>>(Loading())
+
+    init {
+        viewModelScope.launch {
+            userDetail.postValue(repository.getUserById("tadak1"))
+        }
+    }
 }
